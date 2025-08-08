a8fb491a27e3467e9d6405c4c98a4e02a068b0c4
Dante Razo <github.d2brf@simplelogin.fr>
2025-08-08 17:56:22 -0500

    Add cron update script

---
+++ update-stacks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# pull config changes
+echo -e "Pulling from Git repo..."
+cd /apps/
+git pull
+
+# loop through each folder inside /apps/stacks
+for app_dir in /apps/stacks/*/; do
+        cd "$app_dir" || continue
+
+        echo -e "\nProcessing $app_dir..."
+
+        # pull latest images
+        docker compose pull
+
+        # stop containers and remove volumes
+        docker compose down -v
+
+        # start containers in detached mode
+        docker compose up -d
+
+        echo "Done with $app_dir!"
+done
+
+# complete
+echo -e "\nAll done!\n"
+cd ~
