f6a3c1834522af0227bff7e3623f0a8b844202b6
Dante Razo <github.d2brf@simplelogin.fr>
2026-03-07 00:43:43 -0600

    Add new alias script

---
+++ .common-scripts/aliases.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# only define aliases for interactive shells
+case $- in
+	*i*) ;;
+	*) return ;;
+esac
+
+# prefer docker compose v2 syntax
+alias dcp="docker compose pull"
+alias dcd="docker compose down -v"
+alias dcu="docker compose up -d"
+alias dcl="docker compose logs -f"
+alias dcr="docker compose restart"
+
+# list commands
+alias dimg="docker image ls"
+alias dnet="docker network ls"
+alias dvol="docker volume ls"
+
+# compose with project name inferred from folder
+alias dcuu="docker compose up -d --remove-orphans"
+alias dcpb="docker compose build --pull"
