f3646c4b6b2b9f0886d9b94fa5b7b91013981cb5
Dante Razo <github.d2brf@simplelogin.fr>
2026-06-17 07:25:48 -0500

    Use universal function

--- .common/functions.sh
+++ .common/functions.sh
@@ -2,6 +2,5 @@
 dpull() {
 	cd "/docker/$(hostname)"
 	git pull
-    docker compose pull 2>/dev/null || docker compose build
-	docker compose up -d --remove-orphans
+	docker compose up --build -d --remove-orphans
 }
