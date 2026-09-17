4f22271361512bd489f8d9738056309a89221229
Dante Razo <github.d2brf@simplelogin.fr>
2026-09-16 22:40:48 -0500

    Add fix for potential race conditions

--- .common/functions.sh
+++ .common/functions.sh
@@ -6,7 +6,16 @@ dpull() {
 	git pull
 	docker compose pull --ignore-buildable
 	docker compose build --pull --no-cache
-	docker compose up --build -d --remove-orphans
+	if docker compose up --build -d --remove-orphans; then
+		return 0
+	fi
+
+	if [[ "$(basename "$PWD")" == gluetun ]]; then
+		docker compose up --build -d --force-recreate --remove-orphans
+		return $?
+	fi
+
+	return 1
 }
 
 # upgrade packages, then pull
