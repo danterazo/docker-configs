582dca697b0b72c88fbe28f5da0e96cedab5227e
Dante Razo <github.d2brf@simplelogin.fr>
2026-09-21 03:02:46 -0500

    Add comments re: LXC/VM support

--- .common/auto-cd.sh
+++ .common/auto-cd.sh
@@ -6,7 +6,9 @@ APP_DIR="${ROOT_DIR}/$(hostname)"
 
 # move to app dir, if it exists
 if [ -d "$APP_DIR" ]; then
+    # should exist in LXCs
 	cd "$APP_DIR"
 else
+    # should exist in both LXCs and VMs
 	cd "$ROOT_DIR"
 fi
