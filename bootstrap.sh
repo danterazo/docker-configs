ca4b6c48055d554313328516a8d69a7bb0fad928
Dante Razo <github.d2brf@simplelogin.fr>
2026-09-21 05:37:21 -0500

    Make grub update(s) conditional

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,10 +17,12 @@ sudo sed -i 's/^Prompt=.*/Prompt=normal/' /etc/update-manager/release-upgrades
 GRUB_FILE="/etc/default/grub"
 CURRENT_GRUB=$(grep -oP '(?<=^GRUB_CMDLINE_LINUX_DEFAULT=")[^"]*' "$GRUB_FILE")
 CLEANED_GRUB=$(echo "$CURRENT_GRUB" | sed -E 's/\bquiet\b//g' | xargs)	# remove "quiet"
-sudo sed -i -E "s|^GRUB_CMDLINE_LINUX_DEFAULT=\".*\"|GRUB_CMDLINE_LINUX_DEFAULT=\"${CLEANED_GRUB}\"|" "$GRUB_FILE"
 
-# apply grub changes
-sudo update-grub
+# apply grub changes only when the configuration changed
+if [ "${CURRENT_GRUB}" != "${CLEANED_GRUB}" ]; then
+	sudo sed -i -E "s|^GRUB_CMDLINE_LINUX_DEFAULT=\".*\"|GRUB_CMDLINE_LINUX_DEFAULT=\"${CLEANED_GRUB}\"|" "$GRUB_FILE"
+	sudo update-grub
+fi
 
 
 : 'INSTALL PACKAGES'
