cc1e5dc3bb89f33bc289d57609e931d55dba156b
Dante Razo <github.d2brf@simplelogin.fr>
2026-09-21 03:47:29 -0500

    Recursive -> cwd

--- .common/functions.sh
+++ .common/functions.sh
@@ -5,7 +5,6 @@ dpull() {
 	# local variables
 	local root_dir="/docker"
 	local app_dir
-	local compose_file
 
 	# check git init status
 	if [ ! -d "$root_dir/.git" ]; then
@@ -20,13 +19,12 @@ dpull() {
 	# iterate over each app directory and run docker compose commands
 	for app_dir in "$root_dir"/*/; do
 		[ -d "$app_dir" ] || continue
+		[ -f "$app_dir/compose.yaml" ] || [ -f "$app_dir/compose.yml" ] || continue
 
-		while IFS= read -r compose_file; do
-			cd "$(dirname "$compose_file")" || return 1
-			docker compose pull --ignore-buildable || return 1
-			docker compose build --pull --no-cache || return 1
-			docker compose up --build -d --remove-orphans || return 1
-		done < <(find "$app_dir" -type d -name '.*' -prune -o -type f \( -name compose.yaml -o -name compose.yml \) -print | sort)
+		cd "$app_dir" || return 1
+		docker compose pull --ignore-buildable || return 1
+		docker compose build --pull --no-cache || return 1
+		docker compose up --build -d --remove-orphans || return 1
 	done
 }
 
