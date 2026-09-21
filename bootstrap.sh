038e34a153e171a782e6f3bb1fc4acbdf4b38793
Dante Razo <github.d2brf@simplelogin.fr>
2026-09-21 05:28:56 -0500

    Add additional passwordless command

--- bootstrap.sh
+++ bootstrap.sh
@@ -220,7 +220,7 @@ sudo chown -R dante:dante "${ROOT_DIR}"
 
 # grant dante passwordless reboot/poweroff/shutdown/apt
 sudo tee /etc/sudoers.d/dante > /dev/null <<'EOF'
-dante ALL=(root) NOPASSWD: /usr/sbin/reboot, /usr/sbin/poweroff, /usr/sbin/shutdown, /usr/bin/apt-get update, /usr/bin/apt-get dist-upgrade, /usr/bin/apt-get clean, /usr/bin/apt-get autoremove
+dante ALL=(root) NOPASSWD: /usr/sbin/reboot, /usr/sbin/poweroff, /usr/sbin/shutdown, /usr/bin/apt-get update, /usr/bin/apt-get upgrade, /usr/bin/apt-get dist-upgrade, /usr/bin/apt-get clean, /usr/bin/apt-get autoremove
 EOF
 sudo chmod 0440 /etc/sudoers.d/dante
 sudo visudo -c	# validate sudoer file syntax
